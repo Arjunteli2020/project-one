626e10785c44a62510715db1e445c3f841b21f46
Arjunteli2020 <arjunteli@2020gmail.com>
2026-04-06 14:09:11 +0530

    used formeting date

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,17 +1,25 @@
 import "./ExpenseItem.css";
+
+const months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 function ExpenseItem(props) {
-   
-  return  (
-   <div className="expense-item">
-    <div>{props.date.toISOString()}</div>
-     <div className="expense-item__location">{props.location}</div>
-      
-     <div className="expense-item__description">
-        <h2>{props.location}</h2>
+  const month = months[props.date.getMonth()];
+  const year = props.date.getFullYear();
+  const date = props.date.toLocaleString("en-US",{day:"2-digit"});
+
+  return (
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{date}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
-     </div>  
-</div>  
-  );
+      </div>
+    </div>
+  )
 }
 
 export default ExpenseItem;
