d47eacea5a5f93973fe8b9a13fdc78cf8a0194ad
Arjunteli2020 <arjunteli@2020gmail.com>
2026-04-06 10:55:10 +0530

    using Props

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ function App() {
     
         <div>
          <h2>Let's get Started</h2>
-         <ExpenseItem></ExpenseItem>
+         <ExpenseItem date={new Date(2026, 4, 15)} title="Insurance " price="50" location="Bangalore"></ExpenseItem>
+         <ExpenseItem date={new Date(2026, 4, 16)} title="Book" price="20" location="Delhi"></ExpenseItem>
+         <ExpenseItem date={new Date(2026, 4, 17)} title="Pen" price="5" location="Hyderabad"></ExpenseItem>
+         <ExpenseItem date={new Date(2026, 4, 18)} title="Laptop" price="200" location="Mumbai"></ExpenseItem>
         </div>
        
   )
