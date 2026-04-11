472884d67fddbf735dc8228077f3b3fb68af2b22
Arjunteli2020 <arjunteli@2020gmail.com>
2026-04-11 17:20:59 +0530

    Wrapper Components

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,17 +1,16 @@
 import "./ExpenseItem.css";
-import ExpenseDate from "./ExpenseDate"; 
-
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 function ExpenseItem(props) {
- 
   return (
-    <div className="expense-item">
-     <ExpenseDate calendarDate={props.date}></ExpenseDate>
+    <Card className="expense-item">
+      <ExpenseDate calendarDate={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,28 +1,22 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import Card from "./Card";
 
-function Expenses() {
-  const expenses = [
-    { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100 },
-    { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10 },
-    { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1 },
-    { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200 },
-  ];
-
+function Expenses(props) {
   return (
-    <div>
-      
-      {expenses.map((expense, index) => {
+    <Card className="expenses">
+      {props.expenses.map((expense) => {
         return (
           <ExpenseItem
             key={expense.id}
-            date={expense.date}
+            id={expense.id}
             title={expense.title}
+            date={expense.date}
             price={expense.price}
-          ></ExpenseItem>
+          />
         );
       })}
-    </div>
+    </Card>
   );
 }
 
