1bfa65b4ab3a82fb0ec35306303211ed47360afe
Arjunteli2020 <arjunteli@2020gmail.com>
2026-04-17 13:11:10 +0530

    Add conditional statement

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -15,13 +15,11 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  return (
-    <Card className="expenses">
-      <ExpensesFilter
-        selected={filteredYear}
-        onChangeFilter={changeFilterHandler}
-      />
-      {filteredExpenses.map((expense) => {
+  let expensesContent = <p>No expenses found</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = (<>{
+      filteredExpenses.map((expense) => {
         return (
           <ExpenseItem
             key={expense.id}
@@ -30,7 +28,20 @@ const Expenses = (props) => {
             price={expense.price}
           />
         );
-      })}
+      })
+    }
+      {filteredExpenses.length === 1 && (<p>Only one expense found. Please add more</p>)}
+    </>
+    );
+  }
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={changeFilterHandler}
+      />
+      {expensesContent}
     </Card>
   );
 };
